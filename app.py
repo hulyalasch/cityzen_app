234329af49e3336007234011b259c8342a57207b
hulyalasch <43264927+hulyalasch@users.noreply.github.com>
2023-06-02 11:45:37 +0200

    vfhh

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-add_bg_from_local('cityzen_16.jpg')  
+add_bg_from_local('cityzen_17.jpg')  
 
 # Create a Streamlit app
 def main():
