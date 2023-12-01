fa26790f9a47e1c5a9633b2baab34bcd73d22584
hulyalasch <43264927+hulyalasch@users.noreply.github.com>
2023-12-01 17:10:44 +0100

    ok

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import adjustText
 #import wikipedia
 #import wikipediaapi
-
+pip
 # Load the data
 import geopandas as gpd
 import topojson as tp
