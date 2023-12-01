74e62a03df579bf14bc72a6a84f631e2bb0f0d76
hulyalasch <43264927+hulyalasch@users.noreply.github.com>
2023-12-01 22:21:34 +0100

    end

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ def generate_recommendations(landkreise_scaled, selected_features):
 
         ax.axis('off')
 
-        # Display the plot in the respective columnmmmmnpm
+        # Display the plot in the respective column
         with columns[i]:
             st.pyplot(fig)
 
